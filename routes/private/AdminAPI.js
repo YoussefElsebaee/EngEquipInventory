1f0af6264c405f89938a71c1378fbdac9f5d5e8e
YoussefElsebaee <elsebaee.usf@gmail.com>
2024-12-15 01:41:14 +0200

    Update AdminAPI.js

--- routes/private/AdminAPI.js
+++ routes/private/AdminAPI.js
@@ -1,31 +1,48 @@
 const db = require('../../connectors/db');
 
-const { Authenticate, AuthorizedAdmin } = require('../../middleware/auth'); 
+
+const {authMiddleware} = require('../../middleware/auth'); 
+const {getUserId, getUser} = require('../../utils/session'); 
+
+async function checkadmin(req, res){
+
+  const User= getUser(req);
+try {
+    if(User.role != 'standard_user'){
+      return res.status(400).send('Access denied. ONLY users have permission to perform this action.');
+    }
+} catch (error){
+  return res.status().send('error occured during checking the user role');
+}
+}
 
 function handleAdminBackendApi(app) {
+  
     app.get('/api/v1/users/view' , async function(req , res) {
+
+        await getUser(req);
         try{
-          const result = await db.raw(`select * from "SEproject"."users" order by user_id`);
+          const result = await db.raw(`select * from "public"."users" order by user_id`);
           return res.status(200).send(result.rows);
         }catch(err){
           console.log("error message",err.message);
           return res.status(400).send(err.message);
         }
-      });
+      })
     
       app.delete('/api/v1/users/:id', async (req, res)=> {
-        const userId = req.params.id;
-
+        const userId = req.user.id;
+        await getUser(req);
         const trx = await db.transaction();
         try {
-          await trx('SEproject.equipment_order')
-          .join('SEproject.orders', 'equipment_order.order_id', '=', 'orders.order_id')
+          await trx('public.equipment_order')
+          .join('public.orders', 'equipment_order.order_id', '=', 'orders.order_id')
           .where('orders.user_id', userId)
           .del();
-          await trx('SEproject.orders').where('user_id', userId).del();
-          await trx('SEproject.cart').where('user_id', userId).del();
-          await trx('SEproject.rating').where('user_id', userId).del();
-          const result = await trx('SEproject.users').where('user_id', userId).del();
+          await trx('public.orders').where('user_id', userId).del();
+          await trx('public.cart').where('user_id', userId).del();
+          await trx('public.rating').where('user_id', userId).del();
+          const result = await trx('public.users').where('user_id', userId).del();
         
           if (result === 0) {
             await trx.rollback();
@@ -39,27 +56,27 @@ function handleAdminBackendApi(app) {
         }
       
       })
- app.post('/api/v1/equipment/new',  async(req,res) => {
-        UserID= GetUser();
 
-        const {equipmentID,equipment_name,equipment_img,rating,model_number,purchase_date,quantity,status,location,category_ID,supplier_id}= req.body;
+      app.post('/api/v1/equipment/new',  async(req,res) => {
 
-        try{
-            const result = await db.raw(`INSERT INTO "equipments" (equipmentID,equipment_name,equipment_img,rating,model_number,purchase_date,quantity,status,location,category_ID,supplier_id) 
-            VALUES ('${equipmentID}', '${equipment_name}','${equipment_img}','${rating}','${model_number}','${purchase_date}','${quantity}','${status}','${location}','${category_ID}','${supplier_id}');`);
+              const {equipmentID,equipment_name,equipment_img,rating,model_number,purchase_date,quantity,status,location,category_ID,supplier_id}= req.body;
 
-        return res.status(201).json({ message: "Successfully added equipment", rating: result.rows[0] });
-        }
-        
-        catch(err){
-            console.error("Error adding equipment:", err.message);
-        return res.status(500).send("Failed to add equipment");
-        }
+              try{
+                  const result = await db.raw(`INSERT INTO "equipments" (equipmentID,equipment_name,equipment_img,rating,model_number,purchase_date,quantity,status,location,category_ID,supplier_id) 
+                  VALUES ('${equipmentID}', '${equipment_name}','${equipment_img}','${rating}','${model_number}','${purchase_date}','${quantity}','${status}','${location}','${category_ID}','${supplier_id}');`);
 
-    }
-    )
-    app.put('/api/v1/equipment/:id', async(req,res) => {
-        UserID= GetUser();
+              return res.status(201).json({ message: "Successfully added equipment", rating: result.rows[0] });
+              }
+              
+              catch(err){
+                  console.error("Error adding equipment:", err.message);
+              return res.status(500).send("Failed to add equipment");
+              }
+
+      })
+
+      app.put('/api/v1/equipment/:id', async(req,res) => {
+      
         try {
           const {rating , purchase_date, quantity,status,location} = req.body;
           //console.log(req.body,salary); 
@@ -70,7 +87,7 @@ function handleAdminBackendApi(app) {
                             quantity = '${quantity}',
                             status = '${status}',
                             location = '${location}'
-                             where id = ${req.params.id}`
+                            where id = ${req.params.id}`
           const result = await db.raw(query);
           return res.status(200).send("Updated succesfully");
         } 
@@ -88,13 +105,12 @@ function handleAdminBackendApi(app) {
           const result = await db.raw(query);
           return res.status(200).send("deleted succesfully");
         }
-         catch (e) {
+        catch (e) {
           console.log("Error", err.message);
           return res.status(400).send("failed to delete equipment");
         }
       
       })
-
 
 
 
